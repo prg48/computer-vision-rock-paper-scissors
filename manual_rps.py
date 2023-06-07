5d85fff439991ccea0768f7527d58c29628a4ac2
prg48 <32218486+prg48@users.noreply.github.com>
2023-06-07 10:24:51 +0100

    figure out the winner

--- manual_rps.py
+++ manual_rps.py
@@ -8,6 +8,30 @@ def get_user_choice():
     choice = input("Please pick between rock, paper and scissors: ")
     return choice
 
+def get_winner(computer_choice, user_choice):
+    computer_wins = "You lost"
+    player_wins = "You won!"
+    tie = "It is a tie!"
+    if computer_choice == "rock" and user_choice.lower() == "rock":
+        print(tie)
+    elif computer_choice == "rock" and user_choice.lower() == "paper":
+        print(player_wins)
+    elif computer_choice == "rock" and user_choice.lower() == "scissors":
+        print(computer_wins)
+    elif computer_choice == "paper" and user_choice.lower() == "rock":
+        print(computer_wins)
+    elif computer_choice == "paper" and user_choice.lower() == "paper":
+        print(tie)
+    elif computer_choice == "paper" and user_choice.lower() == "scissors":
+        print(player_wins)
+    elif computer_choice == "scissors" and user_choice.lower() == "rock":
+        print(player_wins)
+    elif computer_choice == "scissors" and user_choice.lower() == "paper":
+        print(computer_wins)
+    elif computer_choice == "scissors" and user_choice.lower() == "scissors":
+        print(tie)
+
 if __name__ == "__main__":
     computer_choice = get_computer_choice()
     user_choice = get_user_choice()
+    get_winner(computer_choice, user_choice)
