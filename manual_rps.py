36ae1e7ce3e5c707cbe62363771bf07da3fa8b2b
prg48 <32218486+prg48@users.noreply.github.com>
2023-06-07 10:31:52 +0100

    create a function to simulate the game

--- manual_rps.py
+++ manual_rps.py
@@ -1,14 +1,26 @@
 import random
 
 def get_computer_choice():
+    """
+    This function returns a random choice. either rock, paper or scissors.
+    """
     choices = ["rock", "paper", "scissors"]
     return random.choice(choices)
 
 def get_user_choice():
+    """
+    This function asks the user to enter their choice from either rock, paper or scissors and returns it.
+    """
     choice = input("Please pick between rock, paper and scissors: ")
     return choice
 
 def get_winner(computer_choice, user_choice):
+    """
+    This function decides the winner.
+    Args:
+        computer_choice (str): The choice of the computer.
+        user_choice (str): The choice of the user.
+    """
     computer_wins = "You lost"
     player_wins = "You won!"
     tie = "It is a tie!"
@@ -31,7 +43,13 @@ def get_winner(computer_choice, user_choice):
     elif computer_choice == "scissors" and user_choice.lower() == "scissors":
         print(tie)
 
-if __name__ == "__main__":
+def play():
+    """
+    This function simulates the game of rock, paper and scissors.
+    """
     computer_choice = get_computer_choice()
     user_choice = get_user_choice()
-    get_winner(computer_choice, user_choice)
+    return get_winner(computer_choice, user_choice)
+
+if __name__ == "__main__":
+    play()
