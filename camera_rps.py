8f6997419f54a271c27a60f7ff175e1200fa0ddf
prg48 <32218486+prg48@users.noreply.github.com>
2023-06-07 17:07:31 +0100

    add get_computer_choice and get_winner function

--- camera_rps.py
+++ camera_rps.py
@@ -1,6 +1,7 @@
 import cv2
 from keras.models import load_model
 import numpy as np
+import random
 
 def get_prediction(image, model):
     """
@@ -25,6 +26,48 @@ def get_prediction(image, model):
         return "paper"
     elif max_idx == 3:
         return "scissors"
+    
+def get_computer_choice():
+    """
+    This function returns a random choice. either rock, paper or scissors.
+    """
+    choices = ["rock", "paper", "scissors"]
+    return random.choice(choices)
+
+def get_winner(computer_choice, user_choice):
+    """
+    This function decides the winner.
+    Args:
+        computer_choice (str): The choice of the computer.
+        user_choice (str): The choice of the user.
+    """
+    computer_wins = "You lost"
+    player_wins = "You won!"
+    tie = "It is a tie!"
+    if computer_choice == "rock" and user_choice.lower() == "rock":
+        print(tie)
+    elif computer_choice == "rock" and user_choice.lower() == "paper":
+        print(player_wins)
+    elif computer_choice == "rock" and user_choice.lower() == "scissors":
+        print(computer_wins)
+    elif computer_choice == "paper" and user_choice.lower() == "rock":
+        print(computer_wins)
+    elif computer_choice == "paper" and user_choice.lower() == "paper":
+        print(tie)
+    elif computer_choice == "paper" and user_choice.lower() == "scissors":
+        print(player_wins)
+    elif computer_choice == "scissors" and user_choice.lower() == "rock":
+        print(player_wins)
+    elif computer_choice == "scissors" and user_choice.lower() == "paper":
+        print(computer_wins)
+    elif computer_choice == "scissors" and user_choice.lower() == "scissors":
+        print(tie)
+    elif computer_choice == "rock" and user_choice.lower() == "nothing":
+        print(tie)
+    elif computer_choice == "paper" and user_choice.lower() == "nothing":
+        print(tie)
+    elif computer_choice == "scissors" and user_choice.lower() == "nothing":
+        print(tie)
 
 if __name__ == "__main__":
     # load the model
